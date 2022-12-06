d08ac0eea195791bbf314088b1b9aa052ceb9ba8
Alg0r3 <15010639+Alg0r3@users.noreply.github.com>
2022-12-06 22:47:30 +0100

    feat(framework): install spring dependencies

---
+++ src/test/java/com/alg0r/dashboard/DashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alg0r.dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
